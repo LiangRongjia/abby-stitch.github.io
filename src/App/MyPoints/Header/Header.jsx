d00dd2b64bf2449c6a7f3c461d6780c330f5fdbd
liangrongjia <liangrongjia999@foxmail.com>
2021-03-05 11:06:10 +0800

    Ajax 获取数据，json 文件做数据库

--- src/App/MyPoints/Header/Header.jsx
+++ src/App/MyPoints/Header/Header.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import './Header.css';
-/**
- * @param { { 
- *  avgPoints: string, 
- *  reload: function, 
- *  selectAll: function, 
- *  selectNone: function 
- * } } props 
- */
+import React from 'react'
+import './Header.css'
+import PropTypes from 'prop-types'
+
+Header.propTypes = {
+  avgPoints: PropTypes.number,
+  reload: PropTypes.func,
+  selectNone: PropTypes.func,
+  selectAll: PropTypes.func
+}
+
+Header.defaultProps = {
+  avgPoints: 0,
+  reload: () => { },
+  selectNone: () => { },
+  selectAll: () => { }
+}
+
 export default function Header(props) {
   return (
     <header className="header">
       <img className="avatar" src="./favicon.ico" />
       <h1>少爷的绩点</h1>
-      <p className="avg-points">平均绩点：{props.avgPoints}</p>
+      <p className="avg-points">平均绩点：{props.avgPoints.toFixed(2)}</p>
       <button className="ms-button" onClick={props.reload}>刷新</button>
       <button className="ms-button" onClick={props.selectNone}>全部清除</button>
       <button className="ms-button primary" onClick={props.selectAll}>全部勾选</button>
--- src/App/MyPoints/MyPoints.jsx
+++ src/App/MyPoints/MyPoints.jsx
@@ -10,6 +10,19 @@ const defaultData = { semesters: [], grades: [], classes: [] }
 const defaultAvgPointsState = 0
 const defaultClassesState = [{ semester: '', grades: '', credits: 0, name: '', points: 0, checked: false }]
 
+/**
+ * 根据 gradesName 获取 points
+ * @param { string } gradesName 
+ * @param { {name: string, points: number} [] } grades
+ */
+const getPoints = (gradesName, grades) => grades.filter((item) => item.name === gradesName).map(item => item.points).join('')
+/**
+ * 获取某 semester 下的所有 classItem
+ * @param { number } semesterID 
+ * @param { { semester: string, grades: string, credits: number, name: string, points: number, checked: boolean } [] } classes 
+ */
+const getClasses = (semesterID, classes) => classes.filter(classItem => classItem.semester === semesterID) || defaultClassesState
+
 export default function MyPoints() {
   /* 存储 fetch 数据 */
   const [data, setData] = useState(defaultData)
@@ -21,20 +34,10 @@ export default function MyPoints() {
   useEffect(() => {
     const sum = classesState.reduce((acc, cur) => cur.checked ? acc + cur.credits * cur.points : acc, 0)
     const creditsSum = classesState.reduce((acc, cur) => cur.checked ? acc + cur.credits : acc, 0)
-    const avgPoints = sum === 0 ? '未勾选' : (sum / creditsSum).toFixed(2)
+    const avgPoints = creditsSum === 0 ? 0 : (sum / creditsSum)
     setAvgPointsState(avgPoints)
   }, [classesState]);
 
-  /**
-   * 根据 gradesName 获取 points
-   * @param { string } gradesName 
-   */
-  const getPoints = gradesName => data.grades.filter((item) => item.name === gradesName).map(item => item.points).join('')
-  /**
-   * 获取某 semester 下的所有 classItem
-   * @param { number } semesterID 
-   */
-  const getClasses = semesterID => classesState.filter(classItem => classItem.semester === semesterID) || defaultClassesState
   /**
    * 重新加载该子应用
    */
@@ -42,14 +45,14 @@ export default function MyPoints() {
   /**
    * 勾选全部 classItem
    */
-  const selectAll = () => setClassesState(classesState.map(classItem => ({
+  const checkAll = () => setClassesState(classesState.map(classItem => ({
     ...classItem,
     checked: true
   })))
   /**
    * 取消全部的 classItem 勾选
    */
-  const selectNone = () => setClassesState(classesState.map(classItem => ({
+  const checkNone = () => setClassesState(classesState.map(classItem => ({
     ...classItem,
     checked: false
   })))
@@ -85,7 +88,7 @@ export default function MyPoints() {
   const setGrades = (name, newGrades) => setClassesState(classesState.map(classItem => classItem.name === name ? {
     ...classItem,
     grades: newGrades,
-    points: getPoints(newGrades)
+    points: getPoints(newGrades, data.grades)
   } : classItem))
 
   if (classesState === defaultClassesState) {
@@ -94,7 +97,7 @@ export default function MyPoints() {
       setClassesState(
         newData.classes.map(item => ({
           ...item,
-          points: getPoints(item.grades),
+          points: getPoints(item.grades, data.grades),
           checked: true
         })).sort((a, b) => b.points - a.points === 0 ? b.credits - a.credits : b.points - a.points))
     })
@@ -105,14 +108,14 @@ export default function MyPoints() {
       <Header
         avgPoints={avgPointsState}
         reload={reload}
-        selectAll={selectAll}
-        selectNone={selectNone} />
+        checkAll={checkAll}
+        checkNone={checkNone} />
       {data.semesters.map(semester =>
         <Semester
           key={semester.ID}
           semesterID={semester.ID}
           semesterName={semester.name}
-          classes={getClasses(semester.ID)}
+          classes={getClasses(semester.ID, classesState)}
           gradesList={data.grades.map(item => item.name)}
           setGrades={setGrades}
           selectSemester={selectSemester}
